b8ea706d6c02bd0ab31375b5f1edad0b2e41e3cc
cakirfirat <firatcakir47@gmail.com>
2023-04-25 22:18:57 +0300

    First endpoint deneme

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	. "gamebackend/handlers"
+	// . "gamebackend/helpers"
 
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	log.Println("Server starting...")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/testregister", RegisterHandler).Methods("POST")
+	server := &http.Server{
+		Addr:    ":8096",
+		Handler: r,
+	}
+	server.ListenAndServe()
 }
--- handlers/authHandler.go
+++ handlers/authHandler.go
@@ -1,1 +1,7 @@
 package handlers
+
+import "net/http"
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello word"))
+}
