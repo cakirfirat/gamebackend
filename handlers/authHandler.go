aed08a2fdb577e4de9f0617bc3fa75535df182c7
cakirfirat <firatcakir47@gmail.com>
2023-04-26 13:16:50 +0300

    berk güncelleme

--- handlers/authHandler.go
+++ handlers/authHandler.go
@@ -3,5 +3,5 @@ package handlers
 import "net/http"
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello motherfucker deniz cem yıldız"))
+	w.Write([]byte("hello berk"))
 }
