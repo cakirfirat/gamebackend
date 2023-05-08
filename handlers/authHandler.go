9187c7e4e3d17c47a3ff6839248705f5263f3c0d
cakirfirat <firatcakir47@gmail.com>
2023-05-08 10:41:50 +0300

    Login tamamlandı

--- handlers/authHandler.go
+++ handlers/authHandler.go
@@ -277,7 +277,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if validateUser == nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Kullanıcı bulunamadı."))
+		w.Write([]byte("Kullanıcı adı veya parola hatalı."))
 		return
 	}
 
